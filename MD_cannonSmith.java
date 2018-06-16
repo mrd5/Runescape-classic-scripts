0700871a19c848b016db3b2f4cfc255793fddc48
Mathew Dorish <mathewdorish@hotmail.com>
2018-06-16 15:15:06 -0600

    Added comments

--- MD_cannonSmith.java
+++ MD_cannonSmith.java
@@ -3,6 +3,10 @@ import java.util.Locale;
 import java.text.DecimalFormat;
 import javax.swing.JOptionPane;
 
+//RUN IN FALADOR WEST BANK
+//NOTHING IN INVENTORY (although a sleeping bag, cannon mold, and any number of steel bars would work)
+//SAVE FILE AS MD_cannonSmith.java
+
 public final class MD_cannonSmith extends Script {
 	private static final int MOULD = 1057; //cannon ammo mould id
 	private static final int STEEL_BAR = 171; //steel bar id
@@ -15,8 +19,8 @@ public final class MD_cannonSmith extends Script {
 	private int xp; //smith xp when gaining xp from smithing
 	private int banked_count; //how many cannon balls banked
 
-	private long start_time;
-	private long star_time;
+	private long start_time;//used to display how long script has been running
+	private long star_time;//Used for smithing xp differences
 
 	private final DecimalFormat iformat = new DecimalFormat("#,##0");
 
@@ -25,20 +29,21 @@ public final class MD_cannonSmith extends Script {
 	}
 
 	@Override
-	public void init(String params){
+	public void init(String params){//Initialization, when script is first started
 		start_time = -1L;
 		star_time = -1L;
 		banked_count = 0;
 	}
 
 	@Override
-	public int main(){
-		if (start_time == -1L){
+	public int main(){//This runs over and over for as long as the script is running
+
+		if (start_time == -1L){//Gets current time
 			start_time = System.currentTimeMillis();
 
 		}
 
-		if (star_time == -1L){
+		if (star_time == -1L){//Gets current time and xp of smithing skill
 			star_time = System.currentTimeMillis();
 			start_xp = xp = getXpForLevel(13);
 		} else {
@@ -46,13 +51,12 @@ public final class MD_cannonSmith extends Script {
 		}
 
 
-		if (isQuestMenu()){
+		if (isQuestMenu()){//If talking to a banker, open the bank interface
 			answer(0);
 			return random(1000, 2000);
 		}
 
-		if (isBanking()){
-
+		if (isBanking()){//While bank interface is open, deposit and withdraw necessary items
 			int count = getInventoryCount(CANNON_BALL);
 			if (count > 0){
 				banked_count += count;
@@ -73,31 +77,31 @@ public final class MD_cannonSmith extends Script {
 			return random(1000, 2000);
 		}
 
-		if (isAtApproxCoords(311, 545, 3) && getInventoryCount(STEEL_BAR) > 0){
-			useItemOnObject(STEEL_BAR, 310, 546);
+		if (isAtApproxCoords(311, 545, 3) && getInventoryCount(STEEL_BAR) > 0){//If at furnace with materials to make cannon balls
+			useItemOnObject(STEEL_BAR, 310, 546);//Use steel bar on furnace
 			return random(1000, 2000);
 		}
 
-		if (!isBanking() && getInventoryCount(STEEL_BAR) == 28){
-			walkTo(311, 545);
+		if (!isBanking() && getInventoryCount(STEEL_BAR) == 28){//If in bank with materials to make cannon balls
+			walkTo(311, 545);//Walk to furnace
 			return random(1000, 2000);
 		}
 
 
-		if (getFatigue() > 98){
+		if (getFatigue() > 98){//Sleep if fatigue is almost at 100
 			useSleepingBag();
 			return random(1000, 1500);
 		}
 
-		if (isAtApproxCoords(311, 545, 3) && getInventoryCount(STEEL_BAR) == 0){
-			walkTo(329, 552);
+		if (isAtApproxCoords(311, 545, 3) && getInventoryCount(STEEL_BAR) == 0){//If at furnace but out of materials
+			walkTo(329, 552);//Walk back to bank
 			return random(1000, 2000);
 		}
 
 		
 
-		int[] banker = getNpcByIdNotTalk(BANKERS);
-		if (banker[0] != -1){
+		int[] banker = getNpcByIdNotTalk(BANKERS);//gets nearest banker which is free to talk to and talks to it
+		if (banker[0] != -1){//if banker exists
 			if (distanceTo(banker[1], banker[2]) > 2){
 				if (checkDoors()){
 					return random(1000, 2000);
@@ -114,7 +118,7 @@ public final class MD_cannonSmith extends Script {
 	}
 
 	@Override
-	public void paint(){
+	public void paint(){//Creates a display on screen to show run time, xp gained, and number of cannon balls banked
 		final int orangey = 0xFFD900;
 		final int white = 0xFFFFFF;
 
@@ -139,7 +143,7 @@ public final class MD_cannonSmith extends Script {
 
 
 
-	private String get_runtime() {
+	private String get_runtime() {//Gets run time of script in hours/mins/secs 
        long secs = ((System.currentTimeMillis() - start_time) / 1000L);
        if (secs >= 3600) {
            return (secs / 3600) + " hours, " +
@@ -153,7 +157,7 @@ public final class MD_cannonSmith extends Script {
        return secs + " secs.";
    }
 
-   private String _perHour(int total) {
+   private String _perHour(int total) {//Calculates how many cannon balls are being made per hour
       if (total <= 0 || star_time <= 0L) {
           return "0";
       }
@@ -162,7 +166,7 @@ public final class MD_cannonSmith extends Script {
       );
   }
 
-  	private boolean checkDoors(){
+  	private boolean checkDoors(){//Checks if doors are closed
 		int[] closed = getObjectById(DOORS_CLOSED);
 		if (closed[0] == -1){
 			return false;
